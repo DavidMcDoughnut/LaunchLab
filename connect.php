37e707874a057239150d3947de76c0cf864175f2
David McDonough <dm.mcdonough@gmail.com>
2015-07-14 06:24:59 -0700

    Delete connect.php

--- connect.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-	  $hostname = "crunchiot.db.10718538.hostedresource.com";
-      $username = "crunchiot";
-      $password = "Crunchiot!1";      
-      $databaseName = "crunchiot";
-
-
-?>
